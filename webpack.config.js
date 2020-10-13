645990ee3bcdf230403ca893cc2d24d98bf6d4f9
Christopher F Pulley <christopher.f.pulley@gmail.com>
2020-10-12 21:51:47 -0500

    added webpack and postcss dependencies

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path"); // integrated node package
+
+const postCSSPlugins = [
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("postcss-hexrgba"),
+  require("autoprefixer"),
+];
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js", // starting file
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"), // where bundled.js goes
+  },
+  devServer: {
+    // webpack dev server to all hot content update
+    before: (app, server) => {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 4000,
+    host: "0.0.0.0",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader?url=false",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: postCSSPlugins,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
